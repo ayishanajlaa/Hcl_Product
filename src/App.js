6d26647fb9e31bc90f7f9b4127e6e68a7018a5ec
bindushree-prasanna <bindushree.prasanna@gmail.com>
2020-01-25 07:49:25 +0000

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Footer from "./Components/Footer"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Footer/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/Components/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+
+const footer = () => {
+    return (
+        <>
+            <div>
+                <h3>Easy travel</h3>
+            </div>
+            <div>
+            <img src="src\Data\Images\icon.png" className="App-logo" alt="logo" />
+            </div>
+
+        </>
+    );
+};
+export default footer;
