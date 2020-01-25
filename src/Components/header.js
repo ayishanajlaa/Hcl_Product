c33e81013d6ea6fbce178f10b8cd25a22f807b71
Dhakshana <dhakshana.ramalingam@gmail.com>
2020-01-25 08:32:32 +0000

    header

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,28 +1,15 @@
 import React, {Component} from 'react';
 import Logo from "./logo.png" ;
 
-import {
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem,
-  NavbarText
-} from 'reactstrap';
 
 
 class Header extends Component{
     render(){
          return (
         <div className="header" >
-            <img sizes='40px' src={Logo}></img>
-            </div>
+            <img src={Logo}></img>
+         </div>
+        
         
       );
         
